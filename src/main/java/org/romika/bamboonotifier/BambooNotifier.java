fe4acb137501f215eb7ee2a0b99331a99916f85e
Kubascik, Roman (StMELF) extern <roman.kubascik.extern@stmelf.bayern.de>
2026-07-22 11:38:35 +0200

    Trigger build

--- src/main/java/org/romika/bamboonotifier/BambooNotifier.java
+++ src/main/java/org/romika/bamboonotifier/BambooNotifier.java
@@ -1,5 +1,6 @@
 package org.romika.bamboonotifier;
 
+import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NullMarked;
 import org.romika.bamboonotifier.service.BuildCheckerService;
 import org.springframework.boot.CommandLineRunner;
@@ -7,32 +8,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import lombok.extern.slf4j.Slf4j;
-
+/**
+ * Main
+ */
 @Slf4j
 @SpringBootApplication
 @EnableScheduling
 public class BambooNotifier implements CommandLineRunner {
 
-	private final BuildCheckerService buildCheckerService;
+    private final BuildCheckerService buildCheckerService;
 
-	public BambooNotifier(BuildCheckerService buildCheckerService) {
-		this.buildCheckerService = buildCheckerService;
-	}
+    public BambooNotifier(BuildCheckerService buildCheckerService) {
+        this.buildCheckerService = buildCheckerService;
+    }
 
-	public static void main(String[] args) {
-		// Deactivate headless to have SystemTray support
-		System.setProperty("java.awt.headless", "false");
+    public static void main(String[] args) {
+        // Deactivate headless to have SystemTray support
+        System.setProperty("java.awt.headless", "false");
 
-		SpringApplication app = new SpringApplication(BambooNotifier.class);
-		app.setWebApplicationType(org.springframework.boot.WebApplicationType.NONE);
-		app.run(args);
-	}
+        SpringApplication app = new SpringApplication(BambooNotifier.class);
+        app.setWebApplicationType(org.springframework.boot.WebApplicationType.NONE);
+        app.run(args);
+    }
 
-	@Override
-	@NullMarked
-	public void run(String... args) {
-		// first run after start
-		buildCheckerService.checkBuilds();
-	}
+    @Override
+    @NullMarked
+    public void run(String... args) {
+        // first run after start
+        buildCheckerService.checkBuilds();
+    }
 }
