7f6270c94cf6b895dd7246f0a8d6be64641f24ed
Kubascik, Roman (StMELF) extern <roman.kubascik.extern@stmelf.bayern.de>
2026-07-22 11:44:08 +0200

    Update build to publish exe file

--- src/main/java/org/romika/bamboonotifier/BambooNotifier.java
+++ src/main/java/org/romika/bamboonotifier/BambooNotifier.java
@@ -8,9 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-/**
- * Main
- */
 @Slf4j
 @SpringBootApplication
 @EnableScheduling
